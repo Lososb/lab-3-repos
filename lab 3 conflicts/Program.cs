0304a9dafdb63723e8846720c71fef6e90fa52a8
Lososb <58726460+Lososb@users.noreply.github.com>
2023-12-07 19:14:58 +0300

    first commit

---
+++ lab 3 conflicts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        int x = 1;
+        int y=2;
+        double z = 3.5;
+
+        Console.WriteLine("Hello, World! no conflicts!");
+    }
+}
+
